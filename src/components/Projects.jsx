3ea5d53e351a963262eddbdb78a0766bbf56efda
Mohammed-Abdo-Edries <112798841+Mohammed-Abdo-Edries@users.noreply.github.com>
2023-07-18 19:04:17 +0200

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,6 +13,11 @@ function Projects() {
               <div className="text">restrunt website with the location</div>
               <a href="https://delicious.onrender.com/" target="_blank" rel="noopener noreferrer">Visit</a>
             </div>
+            <div className="project col-sm-10 col-md-5 col-lg-5 text-center" data-aos="flip-left" data-aos-duration="1500">
+              <h2>e commerce website</h2>
+              <div className="text">e commerce website made with vite and tailwind, and also framer motion</div>
+              <a href="https://luxury-t0tu.onrender.com" target="_blank" rel="noopener noreferrer">Visitit</a>
+            </div>
             <div className="add col-sm-10 col-md-5 col-lg-5 text-center" data-aos="flip-left" data-aos-duration="1500">
               <Link to="#contact" smooth>
                 <i className='fa-solid fa-plus'></i>
